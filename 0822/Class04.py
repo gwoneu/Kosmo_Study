821dee5960256c06a87a7797d94dbfeb002c702e
gwoneu <gwoneunji9634@gmail.com>
2023-08-23 12:37:19 +0900

    update code

--- 0822/Class04.py
+++ 0822/Class04.py
@@ -1,20 +1,36 @@
 # 음식(추상클래스) - (피자, 햄버거, 김밥 등)
 from abc import ABC, abstractmethod
 
+
 class Food(ABC):
-    
-    @abstractmethod
-    def order(self):
-        pass
-            
-            
-class Pizza(Food):
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        
-    def order(self):
-        print("주문하신 {} 피자 나왔습니다. 가격은 {}입니다.".format(self.name, self.price))
 
-f1 = Pizza("포테이토",15000)
-f1.order()
+    @abstractmethod
+    def cook(self):
+        pass
+
+
+class Pizza(Food):
+    def __init__(self, name, price, topping, crust):
+        super().__init__(name, price)
+        self._topping = []
+        if (type(self._topping) == list):
+            self._topping += topping
+        else:
+            self._topping.append(topping)
+        self._crust = crust
+
+    def cook(self):
+        print(
+            "{}에서 피자가 나왔습니다. 토핑은 {}이고, 크러스트는 {}입니다. 가격은 {}입니다.".format(self.name, ','.join(self._topping), self._crust, self.price))
+
+    def addTopping(self, topping):
+        self._topping.append(topping)
+
+
+p = Pizza("피자", 100, ["페페로니", "올리브"], "치즈")
+p.addTopping("포테이토")
+
+p.cook()
