79e3c5d03ce1a70f52fe8d12c5cd2be9fa9d252e
gwoneu <gwoneunji9634@gmail.com>
2023-09-11 12:39:34 +0900

    update code

--- class02/src/App.js
+++ class02/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ImportConponent from './comp'
 import Cycle from './sycle'
+import {Sycle} from './cycle'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <p>CSS 적용하기</p>
       <ImportConponent></ImportConponent>
       <Cycle></Cycle>
+      <Sycle />
     </div>
   );
 }
