6453b0ebb9922d025c12ec9da2f1efb5525c9737
gwoneu <gwoneunji9634@gmail.com>
2023-09-11 17:15:47 +0900

    update code, txt

--- class04/src/App.js
+++ class04/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Random from './Random';
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>랜덤 번호 뽑기</p>
+      <Random></Random>
     </div>
   );
 }
--- class04/src/Random.js
+++ class04/src/Random.js
@@ -1,5 +1,29 @@
 import React, {Component} from "react";
 
-export function Random() {
-    
+class Lotto extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {count:0, count2:0};
+        this.handleClick = this.handleClick.bind(this);
+        this.handleClick2 = this.handleClick2.bind(this);
+    }
+
+    handleClick() {
+        this.setState({count: Math.floor(Math.random()*45+1)});
+    }
+
+    handleClick2() {
+        this.setState({count2: Math.floor(Math.random()*45+1)});
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.handleClick}>{this.state.count}</button>
+                <button onClick={this.handleClick2}>{this.state.count2}</button>
+            </div>
+        )
+    }
 }
+
+export default Lotto;
