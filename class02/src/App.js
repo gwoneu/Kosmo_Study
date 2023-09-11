b12fabd5d0e79602197c2d83e12c9ed0e3df6090
gwoneu <gwoneunji9634@gmail.com>
2023-09-11 11:42:10 +0900

    update js

--- class02/src/App.js
+++ class02/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ImportConponent from './comp'
+import Cycle from './sycle'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
       <ImportConponent></ImportConponent>
+      <Cycle></Cycle>
     </div>
   );
 }
