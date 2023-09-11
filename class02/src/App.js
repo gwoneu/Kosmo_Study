bc8da98d99aa2eaf52f52b486b67b22875997680
gwoneu <gwoneunji9634@gmail.com>
2023-09-11 11:27:21 +0900

    new study

---
+++ class02/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ImportConponent from './comp'
+
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <p>CSS 적용하기</p>
+      <ImportConponent></ImportConponent>
+    </div>
+  );
+}
+
+export default App;
