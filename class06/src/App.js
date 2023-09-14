360913c0820f04c12140fdf6d4582b1ee2ccc74d
gwoneu <gwoneunji9634@gmail.com>
2023-09-14 15:10:45 +0900

    update code

--- class06/src/App.js
+++ class06/src/App.js
@@ -4,6 +4,34 @@ import Chat from './Chat'
 import Video from './Video'
 import Product from './Product'
 
+function VideoHome() {
+  return (
+    <>
+      <p>비디오 홈</p>
+    </>
+  );
+}
+
+function VideoList() {
+  return (
+    <p>비디오 리스트 : </p>
+  );
+}
+
+function ChatHome() {
+  return (
+    <>
+      <p>채팅 홈</p>
+    </>
+  );
+}
+
+function ChatList() {
+  return (
+    <p>ChatList</p>
+  )
+}
+
 function App() {
   const productData = [
     {id: 1, name : '물티슈', price: '₩2,000'}, //1번 페이지에 들어가면 물티슈와 가격이 나옴
@@ -17,9 +45,14 @@ function App() {
         {/* 라우트 설정 */}
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/chat" element={<Chat/>}/>
-          <Route path="/video" element={<Video/>}/>
-          <Route path="/product/:id" element={<Product data={productData}/>}/>
+          <Route path="/chat" element={<Chat/>}>
+            <Route index element={<ChatHome/>}/>
+            <Route path="clist" element={<ChatList/>}/>
+          </Route>
+          <Route path="/video" element={<Video/>}>
+            <Route index element={<VideoHome/>}/>
+            <Route path="list" element={<VideoList/>}/>
+          </Route>
         </Routes>
 
         <nav>
@@ -40,7 +73,7 @@ function App() {
           <ul>
             {productData.map((product) => (
               <li key={product.id}>
-                <Link to={`/product/${product.id}`}>{product.name}</Link>
+                <Link to={`/video/product/${product.id}`}>{product.name}</Link>
               </li>
             ))}
           </ul>
--- class06/src/Video.js
+++ class06/src/Video.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 function Video() {
     
     return (
         <div>
             <h1>Video</h1>
+            <Outlet/>
         </div>
     );
 }
