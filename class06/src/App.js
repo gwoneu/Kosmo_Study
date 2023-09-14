962816e4852f76c00fb0bcfdfc72d32e944ecbbb
gwoneu <gwoneunji9634@gmail.com>
2023-09-14 13:18:19 +0900

    Update code

--- class06/src/App.js
+++ class06/src/App.js
@@ -2,8 +2,15 @@ import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import Home from './Home'
 import Chat from './Chat'
 import Video from './Video'
+import Product from './Product'
 
 function App() {
+  const productData = [
+    {id: 1, name : '물티슈', price: '₩2,000'}, //1번 페이지에 들어가면 물티슈와 가격이 나옴
+    {id: 2, name : '커피', price: '₩2,500'},
+    {id: 3, name : '키보드', price: '₩12,000'},
+  ];
+
   return (
     <div>
       <Router>
@@ -12,6 +19,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/chat" element={<Chat/>}/>
           <Route path="/video" element={<Video/>}/>
+          <Route path="/product/:id" element={<Product data={productData}/>}/>
         </Routes>
 
         <nav>
@@ -25,6 +33,16 @@ function App() {
             <li>
               <Link to='/video'>Video</Link>
             </li>
+          </ul>
+        </nav>
+
+        <nav>
+          <ul>
+            {productData.map((product) => (
+              <li key={product.id}>
+                <Link to={`/product/${product.id}`}>{product.name}</Link>
+              </li>
+            ))}
           </ul>
         </nav>
       </Router>
--- class06/src/Video.js
+++ class06/src/Video.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function Video() {
+    
     return (
         <div>
             <h1>Video</h1>
