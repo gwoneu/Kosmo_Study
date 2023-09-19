048e8b80ae09b579a9d4341069bca21ee21b7121
gwoneu <gwoneunji9634@gmail.com>
2023-09-19 15:48:57 +0900

    new study

--- 0918/class01/class01/urls.py
+++ 0918/class01/class01/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello.views import hello_world
+from newapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_world),
+    path('home/', home)
 ]
--- 0918/class01/newapp/models.py
+++ 0918/class01/newapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class NewModel(models.Model):
+    name = models.CharField(max_length=100)
+    skill = models.TextField()
