a49fabadf2649ac72b672c11a3a13aed3d4114a9
gwoneu <gwoneunji9634@gmail.com>
2023-09-19 15:53:25 +0900

    update code

--- 0918/class01/newapp/views.py
+++ 0918/class01/newapp/views.py
@@ -4,5 +4,5 @@ from .models import NewModel
 # Create your views here.
 def home(request):
     models = NewModel.objects.all()
-    # context = {"message" : "MY HOME"}
-    return render(request, 'home.html', {'models' : models})
+    context = {"message" : "MY HOME", 'models' : models}
+    return render(request, 'home.html', context)
