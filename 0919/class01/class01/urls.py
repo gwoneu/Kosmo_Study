956b4e796050985b7ea30ef39199e75565b2f4d1
gwoneu <gwoneunji9634@gmail.com>
2023-09-20 18:23:51 +0900

    update code

--- 0919/class01/class01/urls.py
+++ 0919/class01/class01/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for class01 project.
+URL configuration for class02 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('vote/', include('vote.urls')),
 ]
--- 0919/class02/vote/admin.py
+++ 0919/class02/vote/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin #관리자 사이트를 만들기 위해서
 from .models import Question, Choice #Question과 Choice를 사용할거다
 
-
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fieldsets = ('choice_text', 'votes', 'question') 순서 변경
 
 class ChoiceAdmin(admin.ModelAdmin):
     fieldsets = (
@@ -10,8 +11,24 @@ class ChoiceAdmin(admin.ModelAdmin):
         }),
         ('Question', {
             'fields': ('question',),
-        }), # 파이썬 튜플은 하나짜리는 무조건 , 를 붙인다.
-    )
+        }) # 파이썬 튜플은 하나짜리는 무조건 , 를 붙인다.
+    ) # 그룹묶기
+    
+    list_display = ('choice_text', 'question')
+    list_filter = ('question',)
+    search_fields = ['choice_text']
+
+    
+# class ChoiceAdmin(admin.ModelAdmin):
+#     fieldsets = (
+#         ('Choice', {
+#             'fields': ['choice_text', 'votes'],
+#         }),
+#         ('Question', {
+#             'fields': ['question'], 'classes' : ['collapse']
+#         }), 해당 항목이 숨겨진다.
+#     )
+
 
 admin.site.register(Question) #관리자 사이트에 Question을 등록한다.
 admin.site.register(Choice, ChoiceAdmin) #관리자 사이트에 Choice를 등록한다.
