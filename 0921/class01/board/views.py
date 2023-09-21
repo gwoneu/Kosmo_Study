e290b300bcb7c0563b6b29b34379a5bafa68cf5d
gwoneu <gwoneunji9634@gmail.com>
2023-09-21 19:14:59 +0900

    update code

--- 0921/class01/board/views.py
+++ 0921/class01/board/views.py
@@ -4,8 +4,8 @@ from .models import Post
 # Create your views here.
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'post_list.html', {'pstos' : posts})
+    return render(request, 'post/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post' : post})
+    return render(request, 'post/post_detail.html', {'post' : post})
