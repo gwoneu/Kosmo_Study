aa91bca286105770e557fd524e09cb79dcccc256
gwoneu <gwoneunji9634@gmail.com>
2023-10-17 12:24:16 +0900

    ver05

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var multer = require('multer');
 
-/* 도서검색페이지 */
+//도서이미지 업로드함수
+var upload = multer({
+    storage:multer.diskStorage({
+        destination:(req, file, done)=>{
+            done(null, './public/upload/book')
+        },
+        filename:(req, file, done)=>{
+            var fileName=Date.now() + ".jpg";
+            done(null, fileName);
+        }
+    })
+});
+
+// 도서검색페이지
 router.get('/', function(req, res, next){
     res.render('index', {title:'도서검색', pageName:'books/search.ejs'});
 });
@@ -37,8 +51,10 @@ router.post('/search/insert', function(req, res){
 router.get('/list.json', function(req, res){
     const page=req.query.page;
     const start=(parseInt(page)-1) * 5;
-    const sql='select * from books order by bid desc limit ?,5';
-    db.get().query(sql, [start], function(err, rows){
+    const key=req.query.key;
+    const query=`%${req.query.query}%`;
+    const sql=`select * from books where ${key} like ? order by bid desc limit ?,5`;
+    db.get().query(sql, [query, start], function(err, rows){
         if(err) console.log('도서목록JSON:', err);
         res.send(rows);
     });
@@ -51,10 +67,73 @@ router.get('/list', function(req, res){
 
 //데이터갯수
 router.get('/count', function(req, res){
-    const sql='select count(*) total from books';
-    db.get().query(sql, function(err, rows){
+    const key=req.query.key;
+    const query='%'+req.query.query+'%';
+    const sql=`select count(*) total from books where ${key} like ?`;
+    db.get().query(sql, [query], function(err, rows){
         res.send(rows[0]);
     });
 });
 
+//도서삭제(db관련된 작업은 post로 작업을 많이 한다.-select 제외)
+router.post('/delete', function(req, res){
+    const bid = req.body.bid;
+    const sql = 'delete from books where bid=?'
+    db.get().query(sql, [bid], function(err){
+        if(err) console.log('도서삭제.....', err);
+        res.sendStatus(200); //200번은 성공했을 때
+    });
+});
+
+//도서정보 페이지 이동
+router.get('/read', function(req, res){
+    const bid=req.query.bid;
+    const sql='select *, format(price, 0) fmtprice, date_format(regdate, "%Y-%m-%d") fmtdate from books where bid=?';
+    db.get().query(sql, [bid], function(err, rows){
+        if(err) console.log('도서정보:.........', err);
+        res.render('index', {title:'도서정보', pageName:'books/read.ejs', book:rows[0]});
+    });
+});
+
+//도서정보수정 페이지 이동
+router.get('/update', function(req, res){
+    const bid=req.query.bid;
+    const sql='select *, format(price, 0) fmtprice, date_format(regdate, "%Y-%m-%d") fmtdate from books where bid=?';
+    db.get().query(sql, [bid], function(err, rows){
+        if(err) console.log('도서정보수정:.........', err);
+        res.render('index', {title:'정보수정', pageName:'books/update.ejs', book:rows[0]});
+    });
+});
+
+//도서수정
+router.post('/update', function(req, res){
+    const bid=req.body.bid;
+    const title=req.body.title;
+    const price=req.body.price;
+    const authors=req.body.authors;
+    const publisher=req.body.publisher;
+    const contents=req.body.contents;
+
+    //console.log(bid,title,price,authors,publisher,contents);
+
+    const sql='update books set title=?, price=?, authors=?, publisher=?, contents=? where bid=?';
+    db.get().query(sql, [title, price, authors, publisher, contents, bid], function(err){
+        if(err) console.log('수정 오류: ', err);
+        res.redirect('/books/read?bid=' + bid);
+    })
+});
+
+//이미지 업로드
+router.post('/upload', upload.single('file'), function(req, res){
+    if(req.file){
+        const bid = req.body.bid;
+        //console.log('파일이름:', req.file.filename, bid);
+        const image = '/upload/book/' + req.file.filename;
+        const sql='update books set image=? where bid=?';
+        db.get().query(sql, [image, bid], function(err){
+            if(err) console.log('이미지 업로드 오류', err);
+            res.redirect('/books/read?bid=' + bid);
+        });
+    }
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '홈페이지', pageName:'home.ejs'});
+    res.render('index', { title: '홈페이지', pageName:'home.ejs'});
+});
+
+//지역 검색 페이지 이동
+router.get('/search', function(req, res){
+    res.render('index', {title:'지역검색', pageName:'local/search.ejs'});
 });
 
 module.exports = router;
