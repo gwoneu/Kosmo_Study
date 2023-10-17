16bafcf37429d35800f12b72a51e84a4ac1ca881
gwoneu <gwoneunji9634@gmail.com>
2023-10-17 18:26:55 +0900

    ver06

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,28 @@ router.get('/', function(req, res, next) {
 //지역 검색 페이지 이동
 router.get('/search', function(req, res){
     res.render('index', {title:'지역검색', pageName:'local/search.ejs'});
+});
+
+//도서목록 JSON
+router.get('/books.json', function(req, res){
+    const page=parseInt(req.query.page);
+    const query=`%${req.query.query}%`;
+    // console.log('.............', query);
+    const start=(page-1)*6;
+    const sql=`select * from books where title like ? or authors like ? order by bid desc limit ?,6`;
+    db.get().query(sql, [query, query, start], function(err, rows){ //요기에 배열을 넣어주면 위에 ?로 값이 들어감
+        if(err) console.log('도서목록 JSON : ', err);
+        res.send(rows);
+    });
+});
+
+//도서갯수 출력
+router.get('/count', function(req, res){
+    const query=`%${req.query.query}%`;
+    const sql='select count(*) total from books where title like ? or authors like ?'; //count의 값이 totall로 들어감
+    db.get().query(sql,[query, query], function(err, rows){
+        res.send(rows[0].total.toString());
+    });
 });
 
 module.exports = router;
