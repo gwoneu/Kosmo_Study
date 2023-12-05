ab86b171427e44a904607a08252dd7ce73eb8d9f
gwoneu <gwoneunji9634@gmail.com>
2023-12-05 18:29:14 +0900

    231205수업

--- crawling/ex01.py
+++ crawling/ex01.py
@@ -6,7 +6,7 @@ import streamlit as st
 url = "http://www.cgv.co.kr/movies/?lt=1&ft=0"
 res = requests.get(url)
 res.raise_for_status()
-#pring(res.text)
+#print(res.text)
 
 soup = BeautifulSoup(res.text, 'lxml')
 #title = soup.title.get_text()
